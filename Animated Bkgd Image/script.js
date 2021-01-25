a5b84c98288f458d5fd3d17d05e85f6d34a3a0ff
Fernando Azevedo <azevedo-fernando@users.noreply.github.com>
2021-01-25 13:16:03 -0300

    Comentando

--- Animated Bkgd Image/script.js
+++ Animated Bkgd Image/script.js
@@ -1,7 +1,7 @@
-const banner = document.getElementsByClassName('banner')[0]
+const banner = document.getElementsByClassName('banner')[0] // Pegar o elemento pelo nome da classe e começar pelo Índice 0
 const blocks = document.getElementsByClassName('blocks')
 
-for (var i = 1; i < 400; i++) {
-    banner.innerHTML += "<div class='blocks'></div>"
-    blocks[i].style.animationDelay = `${i * 0.05}s`
+for (var i = 1; i < 400; i++) { 
+    banner.innerHTML += "<div class='blocks'></div>" //adicionando a quantidade de blocos equivalente ao tamanho da página
+    blocks[i].style.animationDelay = `${i * 0.05}s` // adicionando DELAY para que os blocos venham um por vez
 }
